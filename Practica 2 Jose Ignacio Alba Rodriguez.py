5bfdbc92157e63df74fb2dbc9816a2735cb26786
joseialb <joseialb@ucm.es>
2023-03-23 18:43:10 +0100

    Finalizado

--- Practica 2 Jose Ignacio Alba Rodriguez.py
+++ Practica 2 Jose Ignacio Alba Rodriguez.py
@@ -10,15 +10,15 @@ from multiprocessing import Lock, Condition, Process, Manager, Value
 NORTE = 0
 SUR = 1
 
-NCARS = 100
-NPED = 10
+NCARS = 20
+NPED = 100
 
-TIME_CARS_NORTH = 0.5   # Delay de la produccion de vehiculos en direccion norte
-TIME_CARS_SOUTH = 0.5   # Delay de la produccion de vehiculos en direccion sur
-TIME_PED = 5            # Delay de la produccion de peatones
+TIME_CARS_NORTH = 2     # Delay de la produccion de vehiculos en direccion norte
+TIME_CARS_SOUTH = 2     # Delay de la produccion de vehiculos en direccion sur
+TIME_PED = 1            # Delay de la produccion de peatones
 
-TIME_IN_BRIDGE_CARS = (1, 0.5)          # Tiempo que tarda un vehiculo en cruzar el puente en cualquier direccion
-TIME_IN_BRIDGE_PEDESTRIAN = (30, 10)    # Tiempo que tarda una peaton en cruzar el puente en cualquier direccion
+TIME_IN_BRIDGE_CARS = (2, 0.5)          # Tiempo que tarda un vehiculo en cruzar el puente en cualquier direccion
+TIME_IN_BRIDGE_PEDESTRIAN = (20, 5)    # Tiempo que tarda una peaton en cruzar el puente en cualquier direccion
 
 class Monitor():
     def __init__(self):
@@ -73,94 +73,103 @@ class Monitor():
 
     # Funciones de control del Monitor
     
-    def wants_enter_car(self, direccion: int) -> None:
+    def wants_enter_car(self, direccion: int, c_id) -> None:
         with self.mutex:
             self.contador.value += 1
             if direccion == 0:
-                self.N_espera.wait_for(self.puede_esperar_N())
+                print(f"El vehiculo {c_id} N quiere esperar para entrar al puente")
+                self.N_espera.wait_for(self.puede_esperar_N)
+                print(f"El vehiculo {c_id} N está esperando a entrar\n{self}")
                 self.wVehiN.value += 1
                 self.N_puente.wait_for(self.puede_entrar_N)
                 self.wVehiN.value -= 1
                 self.nVehiN.value += 1
-                #self.puente.append(algo)
+                self.puente.append(f"Vehiculo {c_id} N")
+                print(f"El vehiculo {c_id} N ha entrado al puente\n{self}")
                 self.S_espera.notify_all()
                 self.P_espera.notify_all()
             
             else:
-                self.S_espera.wait_for(self.puede_esperar_S())
+                print(f"El vehiculo {c_id} S quiere esperar para entrar al puente")
+                self.S_espera.wait_for(self.puede_esperar_S)
+                print(f"El vehiculo {c_id} S está esperando a entrar\n{self}")
                 self.wVehiS.value += 1
                 self.S_puente.wait_for(self.puede_entrar_S)
                 self.wVehiS.value -= 1
                 self.nVehiS.value += 1
-                #self.puente.append(algo)
+                self.puente.append(f"Vehiculo {c_id} S")
+                print(f"El vehiculo {c_id} S ha entrado al puente\n{self}")
                 self.P_espera.notify_all()
                 self.N_espera.notify_all()
     
-    def leaves_car(self, direccion: int) -> None:
+    def leaves_car(self, direccion: int, c_id: int )-> None:
         with self.mutex:
             self.contador.value += 1
             if direccion == 0:
-                #self.puente remove algo
+                self.puente.remove(f"Vehiculo {c_id} N")
+                print(f"El vehiculo {c_id} N ha salido del puente\n{self}")
                 self.nVehiN.value -= 1
                 self.S_puente.notify_all()
                 self.P_puente.notify_all()
             else:
-                #self.puente remove algo
+                self.puente.remove(f"Vehiculo {c_id} S")
+                print(f"El vehiculo {c_id} S ha salido del puente\n{self}")
                 self.nVehiS.value -= 1
                 self.P_puente.notify_all()
                 self.N_puente.notify_all()
                 
-    def wants_enter_pedestrian(self) -> None:
+    def wants_enter_pedestrian(self, p_id) -> None:
         with self.mutex:
+            print(f"El Peaton {p_id} quiere esperar para entrar al puente")
             self.contador.value += 1
-            self.P_espera.wait_for(self.puede_esperar_P())
+            self.P_espera.wait_for(self.puede_esperar_P)
+            print(f"El Peaton {p_id} está esperando a entrar\n{self}")
             self.wPeat.value += 1
             self.P_puente.wait_for(self.puede_entrar_P)
             self.wPeat.value -= 1
             self.nPeat.value += 1
-            #self.puente.append(algo)
+            self.puente.append(f"Peaton {p_id}")
+            print(f"El Peaton {p_id} ha entrado al puente\n{self}")
             self.N_espera.notify_all()
             self.S_espera.notify_all()
         
-    def leaves_pedestrian(self) -> None:
+    def leaves_pedestrian(self, p_id) -> None:
         with self.mutex:
             self.contador.value += 1
-            #self.puente remove algo
+            self.puente.remove(f"Peaton {p_id}")
+            print(f"El Peaton {p_id} ha salido del puente\n{self}")
             self.nPeat.value -= 1
             self.N_puente.notify_all()
             self.S_puente.notify_all()
 
 
 def delay_car_north() -> None:
-    pass
+    m,d = TIME_IN_BRIDGE_CARS
+    a = random.uniform(m,d)
+    time.sleep(a)
 
 def delay_car_south() -> None:
-    pass
+    m,d = TIME_IN_BRIDGE_CARS
+    a = random.uniform(m,d)
+    time.sleep(a)
 
 def delay_pedestrian() -> None:
-    pass
+    m,d = TIME_IN_BRIDGE_PEDESTRIAN
+    a = random.uniform(m,d)
+    time.sleep(a)
 
-def car(cid: int, direction: int, monitor: Monitor)  -> None:
-    print(f"car {cid} heading {direction} wants to enter. {monitor}")
-    monitor.wants_enter_car(direction)
-    print(f"car {cid} heading {direction} enters the bridge. {monitor}")
-    if direction==NORTH :
+def car(cid: int, direccion: int, monitor: Monitor)  -> None:
+    monitor.wants_enter_car(direccion, cid)
+    if direccion== 0 :
         delay_car_north()
     else:
         delay_car_south()
-    print(f"car {cid} heading {direction} leaving the bridge. {monitor}")
-    monitor.leaves_car(direction)
-    print(f"car {cid} heading {direction} out of the bridge. {monitor}")
+    monitor.leaves_car(direccion, cid)
 
 def pedestrian(pid: int, monitor: Monitor) -> None:
-    print(f"pedestrian {pid} wants to enter. {monitor}")
-    monitor.wants_enter_pedestrian()
-    print(f"pedestrian {pid} enters the bridge. {monitor}")
+    monitor.wants_enter_pedestrian(pid)
     delay_pedestrian()
-    print(f"pedestrian {pid} leaving the bridge. {monitor}")
-    monitor.leaves_pedestrian()
-    print(f"pedestrian {pid} out of the bridge. {monitor}")
-
+    monitor.leaves_pedestrian(pid)
 
 
 def gen_pedestrian(monitor: Monitor) -> None:
@@ -191,8 +200,8 @@ def gen_cars(direction: int, time_cars, monitor: Monitor) -> None:
 
 def main():
     monitor = Monitor()
-    gcars_north = Process(target=gen_cars, args=(NORTH, TIME_CARS_NORTH, monitor))
-    gcars_south = Process(target=gen_cars, args=(SOUTH, TIME_CARS_SOUTH, monitor))
+    gcars_north = Process(target=gen_cars, args=(NORTE, TIME_CARS_NORTH, monitor))
+    gcars_south = Process(target=gen_cars, args=(SUR, TIME_CARS_SOUTH, monitor))
     gped = Process(target=gen_pedestrian, args=(monitor,))
     gcars_north.start()
     gcars_south.start()
@@ -204,3 +213,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    time.sleep(2000)
